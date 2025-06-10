96b213f37ffcf3412d1b97c84e2972c677f3d3e0
carlosmagno02 <carlosmabr44@gmail.com>
2025-06-10 05:06:04 -0300

    feat: add button, card and header components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800'>
+    <div className=''>
 
     </div>
   )
