e732eab07acb033a937c38b4e1b18ce15542786f
carlosmagno02 <carlosmabr44@gmail.com>
2025-06-11 20:52:13 -0300

    feat: ensure recommended products are shown on product details page

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -2,28 +2,45 @@ import { prismaClient } from "@/lib/prisma";
 import ProductImages from "../components/product-images";
 import ProductInfo from "../components/product-info";
 import { computeProductTotalPrice } from "@/helpers/product";
+import ProductList from "@/components/ui/product-list";
 
-interface ProductDetailsPageProps{
-    params:{
-        slug:string
+interface ProductDetailsPageProps {
+    params: {
+        slug: string
     }
 }
 
-const ProductDetailsPage = async ({params:{slug}}:ProductDetailsPageProps) => {
+const ProductDetailsPage = async ({ params: { slug } }: ProductDetailsPageProps) => {
     const product = await prismaClient.product.findFirst({
-        where:{
-            slug:slug,
+        where: {
+            slug: slug,
+        },
+        include: {
+            category: {
+                include: {
+                    products: {
+                        where: {
+                            slug: {
+                                not: slug,
+                            }
+                        }
+                    }
+                }
+            }
         }
     })
 
-    if(!product) return null
+    if (!product) return null
 
-    return ( 
-        <div className="p-5 flex flex-col gap-8">
+    return (
+        <div className="p-5 flex flex-col gap-8 pb-8">
             <ProductImages imageUrls={product.imageUrls} name={product.name} />
-            <ProductInfo products={computeProductTotalPrice(product)}  />
+            <ProductInfo products={computeProductTotalPrice(product)} />
+            <div className="-mx-5">
+                <ProductList products={product.category.products} />
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default ProductDetailsPage;
