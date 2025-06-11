e810c3b64937dbfc7486404ca35211e864edd852
carlosmagno02 <carlosmabr44@gmail.com>
2025-06-11 04:53:22 -0300

    fix: ensure correct product total price is calculated

--- src/helpers/product.ts
+++ src/helpers/product.ts
@@ -12,9 +12,9 @@ export const computeProductTotalPrice = (product:Product): ProductWithTotalPrice
         }
     }
 
-    const totalPrice = Number(product.basePrice) * product.discountPercentage / 100;
+    const totalDiscount = Number(product.basePrice) * product.discountPercentage / 100;
     return{
         ...product,
-        totalPrice,
+        totalPrice: Number(product.basePrice) - totalDiscount,
     }
 }
