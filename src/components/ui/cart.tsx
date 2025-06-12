04e66046cbc33603c5328f36482081cc15d37677
carlosmagno02 <carlosmabr44@gmail.com>
2025-06-12 06:31:47 -0300

    feat: show total, subtotal, and total discount on cart

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -4,9 +4,10 @@ import { useContext } from "react";
 import { CartContext } from "@/providers/cart";
 import CartItem from "./cart-item";
 import { Button } from "./button";
+import { Separator } from "./separator";
 
 const Cart = () => {
-    const { products } = useContext(CartContext)
+    const { products, subtotal, total, totalDiscount } = useContext(CartContext)
     return (
         <div className="flex flex-col gap-5">
             <Badge className="gap-1 w-fit border-primary px-3 text-base uppercase py-[0.375rem] rounded-full" variant='outline'>
@@ -15,11 +16,35 @@ const Cart = () => {
             </Badge>
 
             <div className="flex flex-col gap-5">
-                {products.map((product) => (
-                    <CartItem key={product.id} product={product} />
-                ))}
+                {products.length > 0 ? (
+                    products.map((product) => (
+                        <CartItem key={product.id} product={product} />
+                    ))
+                ) : (
+                    <p className="text-center font-semibold">Seu carrinho está vazio !</p>
+                )}
             </div>
-
+            <div className="flex flex-col gap-3">
+                <Separator />
+                <div className="flex items-center justify-between text-sm">
+                    <p>Subtotal</p>
+                    <p>R${subtotal.toFixed(2)}</p>
+                </div>
+                <Separator />
+                <div className="flex items-center justify-between text-sn">
+                    <p>Entrega</p>
+                    <p>Grátis</p>
+                </div>
+                <Separator />
+                <div className="flex items-center justify-between text-sn">
+                    <p>Descontos</p>
+                    <p>R$ {totalDiscount.toFixed(2)}</p>
+                </div>
+                <div className="flex items-center justify-between font-bold">
+                    <p>Total</p>
+                    <p>R$ {total.toFixed(2)}</p>
+                </div>
+            </div>
         </div>
     )
 }
