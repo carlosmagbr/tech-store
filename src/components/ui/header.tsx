171a149087581f380495a794f75cbd21891619ed
carlosmagno02 <carlosmabr44@gmail.com>
2025-06-12 09:12:43 -0300

    fix: navigate menu

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -64,15 +64,22 @@ const Header = () => {
                             </Button>
 
                         )}
-
-                        <Button variant='outline' className="w-full justify-start gap-2">
-                            <HomeIcon size={16} />
-                            Início
-                        </Button>
-                        <Button variant='outline' className="w-full justify-start gap-2">
-                            <PercentCircleIcon size={16} />
-                            Ofertas
-                        </Button>
+                        <SheetClose asChild>
+                            <Link href='/'>
+                                <Button variant='outline' className="w-full justify-start gap-2">
+                                    <HomeIcon size={16} />
+                                    Início
+                                </Button>
+                            </Link>
+                        </SheetClose>
+                        <SheetClose asChild>
+                            <Link href='/'>
+                                <Button variant='outline' className="w-full justify-start gap-2">
+                                    <PercentCircleIcon size={16} />
+                                    Ofertas
+                                </Button>
+                            </Link>
+                        </SheetClose>
                         <SheetClose asChild>
                             <Link href='/catalog'>
                                 <Button variant='outline' className="w-full justify-start gap-2">
@@ -97,7 +104,7 @@ const Header = () => {
                     </Button>
                 </SheetTrigger>
                 <SheetContent side='right'>
-                    <Cart/>
+                    <Cart />
                 </SheetContent>
             </Sheet>
         </Card>
