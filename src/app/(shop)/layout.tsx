1a64047286f7362f9fe2eb9881ae5711c68fd7ca
carlosmagno02 <carlosmabr44@gmail.com>
2025-08-03 15:22:21 -0300

    refactor: add shop and admin route groups

---
+++ src/app/(shop)/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import '../globals.css'
+import Header from '@/components/ui/header'
+import { AuthProvider } from '@/providers/auth'
+import Footer from '@/components/ui/footer'
+import CartProvider from '@/providers/cart'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Magno Tech Store',
+  description: 'Projeto desenvolvido por Carlos Magno para estudo',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className='flex h-full flex-col'>
+          <AuthProvider>
+            <CartProvider>
+              <Header />
+              <div className='flex-1'>
+                {children}
+              </div>
+              <Footer />
+            </CartProvider>
+          </AuthProvider>
+        </div>
+      </body>
+    </html>
+  )
+}
