e555839769e31af9ec3188f0c41b36c869aa2a02
carlosmagno02 <carlosmabr44@gmail.com>
2025-08-03 16:42:39 -0300

    feat: add initial products table component

--- src/app/(admin)/dashboard/products/page.tsx
+++ src/app/(admin)/dashboard/products/page.tsx
@@ -1,8 +1,34 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { prismaClient } from "@/lib/prisma";
+import { PackageIcon, PlusIcon } from "lucide-react";
+import ProductsTable from "./components/products-table";
+import { computeProductTotalPrice } from "@/helpers/product";
 
-const ProductsPage = () => {
-    return ( 
-        'products'
-     );
+const ProductsPage = async () => {
+    const products = await prismaClient.product.findMany()
+
+    const productsWithTotalPrice = products.map((product) => 
+        computeProductTotalPrice(product)
+    );
+    return (
+        <div className="flex w-full flex-col gap-10 p-10">
+            <Badge className="gap-1 w-fit border-primary px-3 text-base uppercase py-[0.375rem] rounded-full" variant='outline'>
+                <PackageIcon size={18} />
+                Produtos
+            </Badge>
+            <div className="flex w-full items-center justify-between">
+                <p className="text-lg font-bold">
+                    Produtos encontrados:{products.length}
+                </p>
+                <Button className="flex gap-2">
+                    <PlusIcon size={18} />
+                    Adicionar Produto
+                </Button>
+            </div>
+            <ProductsTable products={productsWithTotalPrice}/>
+        </div>
+    );
 }
- 
+
 export default ProductsPage;
